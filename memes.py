e05dfffdab7fe5a1a77ab731bf5c279c7b28c309
git-clone-abhinav <abhi.anu003@gmail.com>
2022-07-11 22:49:07 +0530

    for pawan

--- memes.py
+++ memes.py
@@ -1,3 +1,23 @@
 """
 This module contains the Meme related functions.
 """
+
+import discord
+from discord.ext import commands
+
+# insert a function here that returns a json object of posts.json file.
+
+
+class memes(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    # Commands
+    @commands.command(aliases=['memes'])
+    async def meme(self, ctx):
+        print("Ponged !")
+        await ctx.send(f":eyes: Ping = {round(self.client.latency * 1000)}ms")
+
+def setup(client):
+    client.add_cog(memes(client))
